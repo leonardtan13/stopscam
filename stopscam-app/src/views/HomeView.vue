895a5bc734c42a6a6ede46c500cbd8f9eb0830b3
leonardtan13 <leonardtan13@gmail.com>
2022-02-23 18:17:29 +0800

    add router and firebase config

---
+++ stopscam-app/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <p>
+    Recommended IDE setup:
+    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+    +
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+  </p>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
