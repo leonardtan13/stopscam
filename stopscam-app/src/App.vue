8acbfd3afbb2da114852c3dc5a46ab26881c80c2
leonardtan13 <leonardtan13@gmail.com>
2022-03-05 10:27:44 +0800

    add basic navbar and tailwind configs'

--- stopscam-app/src/App.vue
+++ stopscam-app/src/App.vue
@@ -1,25 +1,17 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import NavBar from "./components/NavBar.vue";
 </script>
 
 <template>
   <div>
-    <nav>
-      <router-link to="/">Home</router-link>
-    </nav>
+    <NavBar />
 
     <router-view></router-view>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/styles/variables.css";
 </style>
---
+++ stopscam-app/src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+</template>
+
+<style scoped></style>
