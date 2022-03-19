a34b1f9f560c0e8cf6eeb441a5d29e4fde965826
triciatll <54834450+triciatll@users.noreply.github.com>
2022-03-20 00:31:07 +0800

    Update Community Feed

--- stopscam-app/src/components/CommunityFeed.vue
+++ stopscam-app/src/components/CommunityFeed.vue
@@ -118,14 +118,14 @@ const data = [
 
 <template>
   <!-- Search bar -->
-  <div class="pt-2 text-gray-600 mb-4 w-3/4 mx-auto">
+  <div class="pt-2 text-gray-600 mb-4 w-3/4 mx-auto h-24">
     <img
       class="inline object-cover w-12 h-12 mr-2 rounded-full ml-64"
       src="https://0.soompi.io/wp-content/uploads/2022/01/11203504/Kim-Tae-Ri2.jpg"
       alt="Profile image"
     />
     <input
-      class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none w-1/2"
+      class="border-2 border-gray-300 bg-white h-full px-5 pr-16 rounded-lg text-sm focus:outline-none w-1/2"
       type="text"
       name="create"
       placeholder="Post something to your community"
@@ -284,16 +284,16 @@ const data = [
   <!-- Page Filter -->
 
   <nav
-    class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800 mb-4 w-1/4 mx-auto"
+    class="bg-white sm:px-4 py-2.5 mx-auto my-4 w-1/3 rounded-xl"
   >
     <div class="container flex flex-wrap justify-evenly items-center mx-auto">
       <div id="mobile-menu" class="hidden w-full md:block md:w-auto">
         <ul
-          class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium"
+          class="flex flex-row mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium "
         >
           <li>
             <button
-              class="block py-2 pr-4 pl-3 rounded md:p-2"
+              class="block py-2 pr-4 pl-3 rounded-lg md:p-2 text-lg"
               :class="ur_selected ? selected_state : unselected_state"
               aria-current="page"
               @click="resetState('review')"
@@ -303,7 +303,7 @@ const data = [
           </li>
           <li>
             <button
-              class="block py-2 pr-4 pl-3 rounded md:p-2"
+              class="block py-2 pr-4 pl-3 rounded-lg md:p-2 text-lg"
               :class="legit_selected ? selected_state : unselected_state"
               @click="resetState('legit')"
             >
@@ -312,7 +312,7 @@ const data = [
           </li>
           <li>
             <button
-              class="block py-2 pr-4 pl-3 rounded md:p-2"
+              class="block py-2 pr-4 pl-3 rounded-lg md:p-2 text-lg"
               :class="scam_selected ? selected_state : unselected_state"
               @click="resetState('scam')"
             >
@@ -324,21 +324,24 @@ const data = [
     </div>
   </nav>
 
-  <!-- Nav Link -->
-  <div class="w-3/4 mx-auto bg-white rounded">
-    <ul class="flex border-b">
-      <li class="-mb-px ml-4 mr-4 p-4">
+
+  <div class="w-2/3 mx-auto bg-white rounded-xl">
+    
+    <!-- Nav Filter -->
+    <ul class="w-5/6 flex gap-2 mx-auto h-18">
+      <li class="px-5 pt-5">
         <a
-          class="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-teal-700 font-semibold"
+          class="font-sans text-xl inline-block default-text"
           href="#"
           >All</a
         >
+        
       </li>
-      <li class="mr-4 p-4">
+      <li class="px-5 pt-5">
         <a
-          class="bg-white inline-block py-2 px-4 text-teal-700 hover:text-teal-800 font-semibold"
+          class="font-sans text-xl inline-block default-text"
           href="#"
-          >Popular</a
+          >Top</a
         >
       </li>
     </ul>
