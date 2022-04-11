b622c144c82bf564248b3073fd02ba048de5c596
triciatll <54834450+triciatll@users.noreply.github.com>
2022-04-11 15:59:13 +0800

    Update HomeView results

--- stopscam-app/src/views/HomeView.vue
+++ stopscam-app/src/views/HomeView.vue
@@ -18,10 +18,12 @@ const error = reactive({
   link: [false, ""],
 });
 
+const isLegit = ref(false);
+const isScam = ref(false);
+const isSimilar = ref(false);
 
 // const link = ref("");
 const similarity = ref({});
-
 const getSimilarity = (currentLink) => {
   const posts = store.posts;
   similarity.value = findSimilarityInPosts(posts, currentLink);
@@ -68,30 +70,47 @@ function inputValidation() {
   }
 }
 
+
 const submitResults = reactive({
   link: "",
-  class: "default"
+  message: ""
+})
+const similarityResults = reactive({
+  link: "",
+  message: "",
+  posts: [], 
 })
 function results(inputLink) {
   console.log("FROM FUNCTION", inputLink, typeof(inputLink))
-  let isLegit = urlIsLegit(inputLink);
-  let isScam = urlIsScam(inputLink);
+  let legit = urlIsLegit(inputLink);
+  let scam = urlIsScam(inputLink);
 
-  console.log("RESULTS:", isLegit, isScam)
+  console.log("RESULTS:", legit, scam, isSimilar.value)
 
-  if(isLegit == "true") {
-    submitResults.link = [inputLink]
-    submitResults.class = "legitPost"
-    return
+  if(legit == true) {
+    isLegit.value = true;
+    console.log("isLegit: ", isLegit.value)
+    submitResults.link = inputLink;
+    submitResults.message = "LEGITIMATE";
+    return;
   } 
-  else if(isScam == "true") {
-    submitResults.link = [inputLink]
-    submitResults.class = "ScamPost"
-    return
+  else if(scam == true) {
+    isScam.value = true;
+    console.log("isScam: ", isScam.value)
+    submitResults.link = inputLink;
+    submitResults.message = "SCAM";
+    return;
   }
   else {
-    
-    console.log(similarity)
+    isSimilar.value = true;
+    getSimilarity(inputLink);
+    console.log("isSimilar: ", isSimilar.value)
+    similarityResults.link = inputLink;
+    similarityResults.message = "We are unable to find this link in our database."
+    similarityResults.posts = similarity.value.ratings
+    console.log(similarityResults.posts.bestMatch)
+    console.log(similarityResults.posts)
+    return;
   }
 
 }
@@ -100,8 +119,8 @@ const submit = () =>  {
   loading.value = true;
   console.log("FROM BUTTON", state.link, typeof(state.link));
   inputValidation();
-  results(state.link)
-
+  results(state.link);
+  loading.value = false;
 }
 </script>
 
@@ -146,10 +165,76 @@ const submit = () =>  {
                   />
         </div>
 
-        <div>
-         
+        <!-- Results Div -->
+        <div class="container w-full mx-auto">
+            <div v-if="isScam" class="transColor w-3/4 mx-auto p-6 rounded-xl">
+              <h1 class="text-4xl text-center my-4 font-bold">
+                {{submitResults.link}}
+              </h1>
+              <p class="text-3xl text-center my-4 ">
+                It is a <span class="text-rose-500 font-semibold">{{submitResults.message}}</span> website!
+              </p>
+              <p class="text-xl text-center mt-8">
+                Verified by people
+              </p>
+              <p class="text-xl text-center"> 
+                Join our community to help us spread words.
+              </p>
+            </div>
 
+            <div v-else-if="isLegit" class="transColor w-3/4 mx-auto p-6 rounded-xl">
+              <h1 class="text-4xl text-center my-4 font-bold">
+                {{submitResults.link}}
+              </h1>
+              <p class="text-3xl text-center my-4 ">
+                It is a <span class="text-emerald-500 font-semibold">{{submitResults.message}}</span> website!
+              </p>
+               <p class="text-xl text-center mt-8">
+                Verified by people
+              </p>
+              <p class="text-xl text-center"> 
+                Join our community to help us spread words.
+              </p>
+            </div>
+            
+            <div v-else-if="isSimilar" class="transColor w-3/4 mx-auto p-6 rounded-xl">
+              <div class="grid-flow-row auto-rows-max justify-items-center">
+                <div>
+                  <h1 class="text-4xl text-center pb-4 font-bold">{{similarityResults.link}}</h1>
+                </div>
+                <div class="text-3xl text-center pb-4">
+                  <h1>{{similarityResults.message}}</h1>
+                </div>
+
+                <div>
+                  <p class="text-2xl">However, your link is</p>
+                  <ul>
+                    <li v-for="post of similarityResults.posts" class="text-xl leading-10">
+                      
+                      <span class="font-semibold">{{(post.rating*100).toFixed(2)}}%</span> 
+                      similar to {{post.target}} which is a 
+                      <span v-if="store.posts.get(post.id).isLegitSite == true" class="text-emerald-500 font-semibold"> Legit</span>
+                      <span v-else class="text-rose-500 font-semibold">Scam</span>
+                      page
+                    </li>
+                  </ul>
+                </div>
+
+              </div>
+
+
+
+
+              
+              
+
+
+
+            </div>
+            <div v-else style="display: none;"></div>
         </div>
+       
+
       </div>
     </div>
 
@@ -157,6 +242,8 @@ const submit = () =>  {
   text: <input v-model="state.link" @change="getSimilarity(state.link)" />
 
   <h2>Similarity: {{ similarity }}</h2>
+
+  <br>
   <button @click="upvotePost('71euK9YfaU9lr5mv3yHU', '49')">Upvote</button>
 
   <div class="container">
@@ -180,5 +267,9 @@ body {
 
 #btn-submit {
   background-color: #cddfd3;
+}
+
+.transColor {
+  background-color: #cddfd359;
 }
 </style>
